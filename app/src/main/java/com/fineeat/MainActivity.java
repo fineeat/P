30847d747913b333fb1e6bd6c62dbc67df3f1ce9
Nicholascwz <Nicholascwz@hotmail.com>
2016-10-12 19:01:11 +0800

    Changed tab title from text too images

--- app/src/main/java/com/fineeat/MainActivity.java
+++ app/src/main/java/com/fineeat/MainActivity.java
@@ -19,19 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //cwz 11/10/16 Added to create tab
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initTab();
+    }
+
+    public void initTab(){
+        //cwz 11/10/16 Added to create tab
         tabLayout = (TabLayout)findViewById(R.id.tabLayout);
         viewPager = (ViewPager)findViewById(R.id.viewPager);
-        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragment(new MainFragment(), "Home");
-        viewPagerAdapter.addFragment(new SearchFragment(), "Search");
-        viewPagerAdapter.addFragment(new FavouriteFragment(), "Favourite");
+
+        viewPagerAdapter = new ViewPagerAdapter(getApplicationContext(), getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new MainFragment(), "Home", R.drawable.home);
+        viewPagerAdapter.addFragment(new SearchFragment(), "Search", R.drawable.search);
+        viewPagerAdapter.addFragment(new FavouriteFragment(), "Favourite", R.drawable.favorite);
+
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
-
     }
 }
--- app/src/main/java/com/fineeat/ViewPagerAdapter.java
+++ app/src/main/java/com/fineeat/ViewPagerAdapter.java
@@ -1,8 +1,14 @@
 package com.fineeat;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -14,14 +20,18 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     ArrayList<Fragment> fragments = new ArrayList<>();
     ArrayList<String> tabTitles = new ArrayList<>();
+    ArrayList<Integer> images = new ArrayList<>();
+    Context context;
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    public ViewPagerAdapter(Context con, FragmentManager fm) {
         super(fm);
+        context = con;
     }
 
-    public void addFragment(Fragment fs, String tts){
+    public void addFragment(Fragment fs, String tts, int img){
         fragments.add(fs);
         tabTitles.add(tts);
+        images.add(img);
     }
 
     @Override
@@ -36,6 +46,14 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return tabTitles.get(position);
+        Drawable image = ContextCompat.getDrawable(context, images.get(position));
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+
+        SpannableString sb = new SpannableString(" ");
+        ImageSpan is = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(is, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        // tab with string title //return tabTitles.get(position);
+        return sb;
     }
 }
